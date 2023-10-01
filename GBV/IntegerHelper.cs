258b640533f981cc9fe01f9b37bf8fb301f0c66c
jrgerba <119472456+jrgerba@users.noreply.github.com>
2023-10-01 17:21:51 -0400

    Helper funcs

---
+++ GBV/IntegerHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace gbv;
+
+public static class IntegerHelper
+{
+    public static (byte high, byte low) SplitShort(ushort value)
+    {
+        (byte high, byte low) bytes;
+
+        bytes.low = (byte)value;
+        bytes.high = (byte)(value >> 8);
+
+        return bytes;
+    }
+
+    public static ushort JoinBytes(byte high, byte low)
+    {
+        return (ushort)((high << 8) | low);
+    }
+}

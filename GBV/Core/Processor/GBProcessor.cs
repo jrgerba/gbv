9b16abe5f4b1a5c056ce692334e12ac4636d73ca
jrgerba <119472456+jrgerba@users.noreply.github.com>
2023-10-10 13:35:58 -0400

    Started basic DMG CPU class

---
+++ GBV/Core/Processor/GBProcessor.cs
@@ -0,0 +1,15 @@
+﻿using GBV.Core.Bus;
+
+namespace GBV.Core.Processor;
+
+public class GBProcessor : ICPU
+{
+    public RegisterPage RegisterPage;
+    private IExecutionEngine eEngine;
+    private IBus bus;
+    
+    public void Clock()
+    {
+        throw new NotImplementedException();
+    }
+}

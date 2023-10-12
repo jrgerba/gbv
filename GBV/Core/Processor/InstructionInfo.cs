b66e4d3ba530ce05f28365f942e222f06e4a2cc2
jrgerba <119472456+jrgerba@users.noreply.github.com>
2023-10-12 13:41:41 -0400

    Added BranchInstructionInfo

--- GBV/Core/Processor/InstructionInfo.cs
+++ GBV/Core/Processor/InstructionInfo.cs
@@ -1,6 +1,6 @@
 ﻿namespace GBV.Core.Processor;
 
-public record InstructionInfo(byte Opcode, CpuFlags FlagMask);
+public record InstructionInfo(byte Opcode, string Name, CpuFlags FlagMask, int BaseTime);
 
-public record BranchInstructionInfo(byte Opcode, CpuFlags FlagMask, int BranchTime) : 
-    InstructionInfo(Opcode, FlagMask);
+public record BranchInstructionInfo(byte Opcode, string Name, CpuFlags FlagMask, int BaseTime, int BranchTime) : 
+    InstructionInfo(Opcode, Name, FlagMask, BaseTime);

343c81d73c1c14f9da900c4eeac745edfead1326
jrgerba <119472456+jrgerba@users.noreply.github.com>
2023-10-13 10:20:15 -0400

    Setting up testing environment

---
+++ ProcessorTesting/DMGEngineTestCPU.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Net;
+using GBV.Core.Processor;
+
+namespace ProcessorTesting;
+
+public class DMGEngineTestCPU : ICPU
+{
+    private DMGEngine _engine = new();
+    private TestBus _bus = new();
+    private RegisterPage _page = new();
+    
+    public void Clock()
+    {
+        byte inst = _bus.ReadByte(_page.PC++);
+
+        _engine.ExecuteInstruction(ref _page, _bus, inst);
+    }
+
+    public void LoadTestRom(string rom)
+    {
+        byte[] file = File.ReadAllBytes(@$"TestRoms\{rom}.gb");
+
+        for (int i = 0; i < 0x2000; i++)
+        {
+            _bus.Write((ushort)i, file[i]);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"A: {_page.A:XX} F {_page.F:XX} B: {_page.B:XX} C: {_page.C:XX} D: {_page.D:XX} E: {_page.E:XX} H: {_page.H:XX} L: {_page.L:XX} SP: {_page.SP:XXXX} PC: 00:{_page.PC:XXXX} ({_bus.ReadByte(_page.PC)} {_bus.ReadByte((ushort)(_page.PC+1))} {_bus.ReadByte((ushort)(_page.PC+2))} {_bus.ReadByte((ushort)(_page.PC+3))})";
+    }
+}
---
+++ ProcessorTesting/TestBus.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GBV;
+using GBV.Core.Bus;
+
+namespace ProcessorTesting;
+
+public class TestBus : IBus
+{
+    private byte[] _data = new byte[0x10000];
+
+    public byte ReadByte(ushort address) => _data[address];
+
+    public ushort ReadShort(ushort address)
+    {
+        byte low = ReadByte(address++);
+        byte high = ReadByte(address);
+
+        return IntegerHelper.JoinBytes(high, low);
+    }
+
+    public void Write(ushort address, byte value)
+    {
+        if (address == 0xFF01)
+            Console.Write((char)value);
+        
+        _data[address] = value;
+    }
+
+    public void Write(ushort address, ushort value)
+    {
+        (byte high, byte low) = IntegerHelper.SplitShort(value);
+        
+        Write(address++, low);
+        Write(address, high);
+    }
+}

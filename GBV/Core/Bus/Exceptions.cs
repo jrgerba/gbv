4af8fdbfd36708013406273a0d00e311d663cb04
jrgerba <119472456+jrgerba@users.noreply.github.com>
2023-10-18 14:42:55 -0400

    Added UnhandledAddressException for bus r/w

---
+++ GBV/Core/Bus/Exceptions.cs
@@ -0,0 +1,9 @@
+﻿namespace GBV.Core.Bus;
+
+public class UnhandledAddressException : Exception
+{
+    private const string DefaultMessage = "The specified address is not handled in this context";
+    
+    public UnhandledAddressException(string message = DefaultMessage) :
+        base(message) {}
+}

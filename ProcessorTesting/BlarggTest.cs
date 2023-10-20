b8f56ddf1bd59758122b259d9218643d29daf09d
jrgerba <119472456+jrgerba@users.noreply.github.com>
2023-10-19 21:53:33 -0400

    Reworked testing environment

--- ProcessorTesting/BlarggTest.cs
+++ ProcessorTesting/BlarggTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using GBV.Core.Bus;
 using GBV.Core.Processor;
 using NUnit.Framework;
 
@@ -10,84 +11,106 @@ namespace ProcessorTesting;
 
 public class Tests
 {
-    private DMGEngineTestCPU _cpu;
-    
+    private MainBus Bus;
+    private byte[][] TestRoms = new byte[11][];
+    private StreamReader[] TestLogs = new StreamReader[11];
+
     [SetUp]
     public void Setup()
     {
-        _cpu = new DMGEngineTestCPU();
+        Bus = new MainBus();
+
+        Bus.AttachCpu(new GBProcessor(Bus));
+        Bus.AttachTimer(new Timer());
+        Bus.AttachInterruptHandler(new InterruptHandler(Bus));
+        
+        Bus.Reset();
+
+        for (int i = 1; i < 12; i++)
+        {
+            TestRoms[i - 1] = File.ReadAllBytes($"TestRoms/blargg{i}.gb");
+            TestLogs[i - 1] = new StreamReader(File.OpenRead($"TestLogs/blargg{i}.txt"));
+        }
     }
 
-    public void TestGeneral(string testStr)
+    private (bool, int) CompareTest(StreamReader reader, bool ignoreBoot = true)
     {
-        StreamReader f = new StreamReader(File.OpenRead($"TestLogs/{testStr}.txt"));
-        _cpu.LoadTestRom(testStr);
-
-        int line = 0;
+        int count = 0;
+        string oldTest = "", oldGood = "";
         
-        string goodLogOld = "";
-        string myLogOld = "";
-        
-        while (!f.EndOfStream)
+        while (!reader.EndOfStream)
         {
-            string myLog = _cpu.ToString();
-            string goodLog = f.ReadLine();
-            line++;
-
-            try
+            if (Bus.Processor.RegisterPage.PC < 0x100 && ignoreBoot)
             {
-                if (_cpu.Page.PC >= 0x100)
-                    Assert.That(goodLog, Is.EqualTo(myLog));
+                Bus.Clock();
+                continue;
             }
-            catch (AssertionException e)
+            
+            string test = Bus.Processor.ToString(), 
+                good = reader.ReadLine();
+
+
+            if (test != good)
             {
-                Console.WriteLine($"Executed {line} instructions");
-                Console.WriteLine($"$Logs did not match:\nGood: {goodLog}\nTest: {myLog}");
-                Console.WriteLine($"Previous state:\n\tGood: {goodLogOld}\n\tTest: {myLogOld}");
-                throw;
+                Console.WriteLine($"Good case: {good}\nTest case: {test}");
+                Console.WriteLine($"\tPrevious State:\n\tGood case: {oldGood}\n\tTest case: {oldTest}");
+                return (false, count);
             }
 
-            if (_cpu.Page.PC >= 0x100)
-            {
-                goodLogOld = goodLog;
-                myLogOld = myLog;
+            Bus.Clock();
+            count++;
 
-            }            _cpu.Clock();
+            oldTest = test;
+            oldGood = good;
         }
-        Console.WriteLine($"Executed {line} instructions");
+
+        return (true, count);
+    }
+    
+    private void TestGeneral(int tCase, bool ignoreBoot = true)
+    {
+        int i = 0;
+        foreach (byte b in TestRoms[tCase])
+            Bus.Write((ushort)i++, b);
+
+        (bool passed, int count) = CompareTest(TestLogs[tCase], ignoreBoot);
+        
+        Console.WriteLine($"Executed {count} instructions");
+        
+        Assert.That(passed, Is.True);
     }
 
     [Test]
-    public void Test1() => TestGeneral("blargg1");
+    public void Test1() => TestGeneral(0);
     [Test]
     public void Test2()
     {
-        _cpu.Page.PC++;
-        TestGeneral("blargg2");
+        Bus.Processor.RegisterPage.PC++;
+        TestGeneral(1, false);
     }
 
     [Test]
-    public void Test3() => TestGeneral("blargg3");
+    public void Test3() => TestGeneral(2);
     [Test]
-    public void Test4() => TestGeneral("blargg4");
+    public void Test4() => TestGeneral(3);
     [Test]
-    public void Test5() => TestGeneral("blargg5");
+    public void Test5() => TestGeneral(4);
 
     [Test]
     public void Test6()
     {
-        _cpu.Page.PC++;
-        TestGeneral("blargg6");
+        Bus.Processor.RegisterPage.PC++;
+        TestGeneral(5);
     }
 
     [Test]
-    public void Test7() => TestGeneral("blargg7");
+    public void Test7() => TestGeneral(6);
     [Test]
-    public void Test8() => TestGeneral("blargg8");
+    public void Test8() => TestGeneral(7);
     [Test]
-    public void Test9() => TestGeneral("blargg9");
+    public void Test9() => TestGeneral(8);
     [Test]
-    public void Test10() => TestGeneral("blargg10");
+    public void Test10() => TestGeneral(9);
     [Test]
-    public void Test11() => TestGeneral("blargg11");
+    public void Test11() => TestGeneral(10);
 }

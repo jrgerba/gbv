a6abcb027f7c6c783f1aea299598673936d08315
jrgerba <119472456+jrgerba@users.noreply.github.com>
2023-10-19 21:52:50 -0400

    Expanded IBus interface to require console components

--- GBV/Core/Bus/IBus.cs
+++ GBV/Core/Bus/IBus.cs
@@ -1,10 +1,27 @@
-﻿namespace GBV.Core.Bus;
+﻿using GBV.Core.Processor;
+using Timer = GBV.Core.Processor.Timer;
+
+namespace GBV.Core.Bus;
 
 public interface IBus
 {
+    public ICPU Processor { get; }
+    public Timer Timer { get; }
+    public InterruptHandler InterruptHandler { get; }
+    public int WorkTime { get; set; }
+    
     byte ReadByte(ushort address);
+    
     ushort ReadShort(ushort address);
-
+    
     void Write(ushort address, byte value);
+    
     void Write(ushort address, ushort value);
+
+    public void Clock();
+
+    public void AttachCpu(ICPU processor);
+    public void AttachTimer(Timer timer);
+    public void AttachInterruptHandler(InterruptHandler handler);
+    
 }
